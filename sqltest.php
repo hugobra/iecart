dcffdf55051089af2029fe7110ecd72143ae5d42
hugobra <hugo.branly@gadz.org>
2017-07-27 11:18:27 +0200

    Delete sqltest.php

--- sqltest.php
+++
@@ -1,9 +0,0 @@
-<?php
-$connexion = new PDO('mysql:host=localhost;dbname=testsql', 'root', '');
-
-$queryFap = $connexion->query("SELECT * FROM fap");
-while ($fetchFap = $queryFap->fetch()){
-	
-	echo "<hr>";
-}
-?>
